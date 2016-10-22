ae79755f77cd8eed59aaf46b152a23e1fa2c0024
Evan Miller <emmiller@gmail.com>
2016-10-22 09:39:39 +0300

    SAS: Support Win Baltic code page (Windows-1257)

--- src/sas/readstat_sas.c
+++ src/sas/readstat_sas.c
@@ -50,6 +50,7 @@ static readstat_charset_entry_t _charset_table[] = {
     { .code = 64,    .name = "WINDOWS-1254" },
     { .code = 65,    .name = "WINDOWS-1255" },
     { .code = 66,    .name = "WINDOWS-1256" },
+    { .code = 67,    .name = "WINDOWS-1257" },
     { .code = 119,   .name = "EUC-TW" },
     { .code = 123,   .name = "BIG-5" },
     { .code = 125,   .name = "EUC-CN" },
