250b8ce7e8ce3aae4cb0fa3b17a287a87f5585fa
Evan Miller <emmiller@gmail.com>
2018-12-24 12:45:25 -0500

    Fix crash

--- src/txt/readstat_schema.c
+++ src/txt/readstat_schema.c
@@ -5,8 +5,10 @@
 #include "readstat_copy.h"
 
 void readstat_schema_free(readstat_schema_t *schema) {
-    free(schema->entries);
-    free(schema);
+    if (schema) {
+        free(schema->entries);
+        free(schema);
+    }
 }
 
 readstat_schema_entry_t *readstat_schema_find_or_create_entry(readstat_schema_t *dct, const char *var_name) {
