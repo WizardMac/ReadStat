ae0791db6724fdb78cb6103057226b7d3877a1a4
Mikko Marttila <mikmart@mikkomarttila.com>
2019-07-14 00:54:30 +0300

    Same for compressed sav
    This is a problem if row_limit can be 0 when not set.

--- src/spss/readstat_sav_read.c
+++ src/spss/readstat_sav_read.c
@@ -889,7 +889,9 @@ static readstat_error_t sav_read_compressed_data(sav_ctx_t *ctx,
                 uncompressed_offset = 0;
             }
 
-            if (state.status == SAV_ROW_STREAM_FINISHED_ALL || ctx->current_row == ctx->row_limit)
+            if (state.status == SAV_ROW_STREAM_FINISHED_ALL)
+                goto done;
+            if (ctx->row_limit > 0 && ctx->current_row == ctx->row_limit)
                 goto done;
         }
     }
