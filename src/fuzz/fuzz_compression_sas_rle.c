f273565b3678066c2c9a4055dac4049438a92a1e
Evan Miller <emmiller@gmail.com>
2020-04-23 08:58:39 -0400

    SAS RLE compressor: Large file fixes.
    Insertion runs (repeated characters) and copy runs (non-repeated
    characters) of >4kb were not correctly compressed.
    
    Also use heap allocation for the fuzzing target so that we don't run out
    of stack space.
    
    Fixes #201
    
    Thanks to Google Autofuzz project for the report.

--- src/fuzz/fuzz_compression_sas_rle.c
+++ src/fuzz/fuzz_compression_sas_rle.c
@@ -9,16 +9,18 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     if (compressed_len <= 0 || Size == 0)
         return 0;
 
-    uint8_t compressed[compressed_len];
-    uint8_t decompressed[Size];
+    uint8_t *compressed = malloc(compressed_len);
+    uint8_t *decompressed = malloc(Size);
 
-    if (sas_rle_compress(compressed, compressed_len, Data, Size) != compressed_len) {
-        printf("Unexpected compressed size\n");
+    ssize_t actual_len = 0;
+
+    if ((actual_len = sas_rle_compress(compressed, compressed_len, Data, Size)) != compressed_len) {
+        printf("Unexpected compressed size (Expected: %ld  Got: %ld)\n", compressed_len, actual_len);
         __builtin_trap();
     }
 
-    if (sas_rle_decompress(decompressed, Size, compressed, compressed_len) != Size) {
-        printf("Unexpected decompressed size\n");
+    if ((actual_len = sas_rle_decompress(decompressed, Size, compressed, compressed_len)) != Size) {
+        printf("Unexpected decompressed size (Expected: %ld  Got: %ld)\n", Size, actual_len);
         __builtin_trap();
     }
 
@@ -26,6 +28,9 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
         printf("Decompressed data doesn't match original\n");
         __builtin_trap();
     }
+
+    free(compressed);
+    free(decompressed);
 
     return 0;
 }
