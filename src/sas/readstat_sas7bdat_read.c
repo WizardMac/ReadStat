b44d62c2a664bc8b83673508423c282f66fb6903
Evan Miller <emmiller@gmail.com>
2020-08-25 21:26:31 -0400

    Add bounds checking to SAS binary decompressor

--- src/sas/readstat_sas7bdat_read.c
+++ src/sas/readstat_sas7bdat_read.c
@@ -492,23 +492,32 @@ cleanup:
 }
 
 static readstat_error_t sas7bdat_parse_subheader_rdc(const char *subheader, size_t len, sas7bdat_ctx_t *ctx) {
-    /* TODO bounds checking */
     readstat_error_t retval = READSTAT_OK;
     const unsigned char *input = (const unsigned char *)subheader;
     char *buffer = malloc(ctx->row_length);
     char *output = buffer;
-    while (input - (const unsigned char *)subheader < len - 2) {
+    while (input + 2 <= (const unsigned char *)subheader + len) {
         int i;
         unsigned short prefix = (input[0] << 8) + input[1];
         input += 2;
         for (i=0; i<16; i++) {
-            if (input - (const unsigned char *)subheader >= len)
-                break;
-
             if ((prefix & (1 << (15 - i))) == 0) {
+                if (input + 1 > (const unsigned char *)subheader + len) {
+                    break;
+                }
+                if (output + 1 > buffer + ctx->row_length) {
+                    retval = READSTAT_ERROR_ROW_WIDTH_MISMATCH;
+                    goto cleanup;
+                }
                 *output++ = *input++;
                 continue;
             }
+
+            if (input + 2 > (const unsigned char *)subheader + len) {
+                retval = READSTAT_ERROR_PARSE;
+                goto cleanup;
+            }
+
             unsigned char marker_byte = *input++;
             unsigned char next_byte = *input++;
             size_t insert_len = 0, copy_len = 0;
@@ -519,9 +528,17 @@ static readstat_error_t sas7bdat_parse_subheader_rdc(const char *subheader, size
                 insert_len = 3 + marker_byte;
                 insert_byte = next_byte;
             } else if ((marker_byte >> 4) == 1) {
+                if (input + 1 > (const unsigned char *)subheader + len) {
+                    retval = READSTAT_ERROR_PARSE;
+                    goto cleanup;
+                }
                 insert_len = 19 + (marker_byte & 0x0F) + next_byte * 16;
                 insert_byte = *input++;
             } else if ((marker_byte >> 4) == 2) {
+                if (input + 1 > (const unsigned char *)subheader + len) {
+                    retval = READSTAT_ERROR_PARSE;
+                    goto cleanup;
+                }
                 copy_len = 16 + (*input++);
                 back_offset = 3 + (marker_byte & 0x0F) + next_byte * 16;
             } else {
@@ -530,11 +547,19 @@ static readstat_error_t sas7bdat_parse_subheader_rdc(const char *subheader, size
             }
 
             if (insert_len) {
+                if (output + insert_len > buffer + ctx->row_length) {
+                    retval = READSTAT_ERROR_ROW_WIDTH_MISMATCH;
+                    goto cleanup;
+                }
                 memset(output, insert_byte, insert_len);
                 output += insert_len;
             } else if (copy_len) {
                 if (output - buffer < back_offset || copy_len > back_offset) {
                     retval = READSTAT_ERROR_PARSE;
+                    goto cleanup;
+                }
+                if (output + copy_len > buffer + ctx->row_length) {
+                    retval = READSTAT_ERROR_ROW_WIDTH_MISMATCH;
                     goto cleanup;
                 }
                 memcpy(output, output - back_offset, copy_len);
