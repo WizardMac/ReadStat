3c26f258bfd573fafcbef98ba68e63487a5ddb93
github@rplusplus.com <jerome.richard@rplusplus.com>
2020-09-21 11:44:05 +0200

    More advanced support of unicode paths for windows

---
+++ src/bin/util/main.h
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// True main for all platforms
+int portable_main(int argc, char *argv[]);
+
+
+#if defined _WIN32
+    // Standard way of decoding wide-string command-line arguments one Windows.
+    // Call portable_main with UTF-8 strings.
+    int wmain(int argc, wchar_t *argv[]) {
+        int ret = 1;
+        char** utf8_argv = calloc(argc, sizeof(char*));
+
+        for (int i=0; i<argc; ++i) {
+            const size_t len = wcstombs(NULL, argv[i], 0);
+
+            if (len == (size_t)-1) {
+                fprintf(stderr, "Fatal error: command line encoding failure (argument %d)\n", i+1);
+                goto cleanup;
+            }
+
+            utf8_argv[i] = malloc(len + 1);
+            const size_t ret = wcstombs(utf8_argv[i], argv[i], len);
+
+            if (len == (size_t)-1) {
+                fprintf(stderr, "Fatal error: command line encoding failure (argument %d)\n", i+1);
+                goto cleanup;
+            }
+
+            utf8_argv[i][len] = 0;
+        }
+
+        ret = portable_main(argc, argv);
+
+    cleanup:
+        for(int i=0; i<argc; ++i)
+            free(utf8_argv[i]);
+
+        free(utf8_argv);
+        return ret;
+    }
+#else
+    // Proxy main.
+    // On Mac, argv encoding is the current local encoding.
+    // On Linux, argv encoding is difficult to know, but it 
+    // should often be the current local encoding (generally UTF-8).
+    int main(int argc, char *argv[])
+    {
+        portable_main(argc, argv);
+        return 0;
+    }
+#endif
+
