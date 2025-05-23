8818d206fc183247104662b25bf7fa19f0b87ca6
Evan Miller <emmiller@gmail.com>
2025-05-23 09:44:02 -0400

    SAS7BDAT: set default informat

--- src/sas/readstat_xport_write.c
+++ src/sas/readstat_xport_write.c
@@ -123,8 +123,9 @@ static readstat_error_t xport_write_variables(readstat_writer_t *writer) {
                 any_has_long_format = 1;
                 needs_long_record = 1;
             }
-        } else if (variable->display_width) {
+        } else {
             namestr.nfl = variable->display_width;
+            namestr.nfd = variable->decimals;
         }
 
         if (variable->informat[0]) {
